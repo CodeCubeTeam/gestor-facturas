fd482b73e245316b67940f717e7e3d87c9a9fc1b
cllado <cllado@xmltravelgate.com>
2018-09-27 17:05:26 +0200

    form for items bill

--- src/app/components/form-facturas/form-facturas.component.ts
+++ src/app/components/form-facturas/form-facturas.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Item {
+  value: string;
+  viewValue: string;
+}
+export interface Quantity {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-form-facturas',
   templateUrl: './form-facturas.component.html',
   styleUrls: ['./form-facturas.component.scss']
 })
 export class FormFacturasComponent implements OnInit {
+
+  items: Item[] = [
+    {value: 'items-0', viewValue: 'Items1'},
+    {value: 'items-1', viewValue: 'Items2'},
+    {value: 'items-2', viewValue: 'Items3'}
+  ];
+  quantitys: Quantity[] = [
+    {value: '1', viewValue: '1'},
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'}
+  ];
 
   constructor() { }
 
